3b049aee1f6aa8ed231cb555b09d90411d2b75b4
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2021-11-23 11:42:30 -0700

    add the email into link

--- skibumnft/src/components/content/Footer.js
+++ skibumnft/src/components/content/Footer.js
@@ -9,8 +9,8 @@ export default function Footer() {
     return (
       <div className="h-100 grid grid-rows-5 bg-black h-12 w-full relative text-white" style={{ backgroundImage: `url('${bgimg}` }}>
 
-        <div  className="row-start-2 bg-fixed bg-black bg-cover h-20 relative">                   
-      <p>Patrol@skibumnft.com</p>
+        <div  className="row-start-3 p-5 bg-fixed bg-black bg-cover h-20 relative">                   
+      <a href="mailto:Patrol@skibumnft.com"  className="text-2xl">Patrol@skibumnft.com</a>
       </div>
       </div>
       
