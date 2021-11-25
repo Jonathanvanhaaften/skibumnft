bc1f3521b489aa9883f07da79e9e7450f0d0b9aa
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2021-11-25 14:04:29 -0700

    edited links page basic needs work

--- skibumnft/src/components/content/Links.js
+++ skibumnft/src/components/content/Links.js
@@ -4,19 +4,33 @@ import React, {Fragment} from 'react'
 export default function Links() {
     return (
         <Fragment>
-            <div className='aspect-w-16 aspect-h-9'>
+            <div className='text-white  p-20'>
+            <h1 className="text-5xl">Our Friends and sites of intrest</h1>
+            <p className="text-xl">Any NFTs premoted on our site are for astic and for the community not for 
+                financial gain. 
+            </p>
+            <p className="text-xl"> We would like to premote art and community.</p>
+            </div>
+            <div className="text-right text-4xl m-8 text-gray-600">
+                <a className=" hover:text-white p-5" href="https://www.pixelbeachclub.com/">Pixel Beach Club - Artist</a><br />
+                <a className=" hover:text-white p-5" href="https://www.wildwoodcafe.ca/">Wild Wood Cafe, Whistler BC </a><br />
+                <a className=" hover:text-white p-5" href="https://www.maurmere.com">Maur Mere Media </a><br />
+                <a className=" hover:text-white p-5" href="https://foundation.app/@cavidmdw">Cavid Mammadli's Foundation site - Artist</a><br />
+                <a className=" hover:text-white p-5" href="https://blackfeather.com/hike">Black Feather The wilderness adventure company</a><br />
+                <a className=" hover:text-white p-5" href="https://boredapeyachtclub.com/#/">The Bored Ape Yacht Club</a><br />
+                <a className=" hover:text-white p-5" href="https://www.blockchainminers.club/">Blockchain Miners</a><br />
+                <a className=" hover:text-white p-5" href="https://protectourwinters.org/pow-international/">POW Protect Our Winters</a><br />
+                <a className=" hover:text-white p-5" href="https://lisafogarty.se/">Lisa Fogarty - Artist</a><br />
+                <a className=" hover:text-white p-5" href="https://hashlips.online/HashLips">HashLips - YouTube'er</a><br />
+                <a className=" hover:text-white p-5" href="https://skookumcycleandski.com">Skookum Cycle and Ski, Revelstoke BC</a><br />
+                <a className=" hover:text-white p-5" href="https://www.splitzgrill.com/">Splitz Grill, Whistler BC</a><br />
+                <a className=" hover:text-white p-5" href="http://www.thevillageidiot.ca/">The Village Idiot, Bar and Grill, Revelstoke BC </a><br />
+                <a className=" hover:text-white p-5" href="https://www.whistlerblackcomb.com/explore-the-resort/the-village/dining/the-glc.aspx">GLC Bar and Grill, Whistler BC </a><br />
+                <a className=" hover:text-white p-5" href="https://www.sandkexpeditions.com">S and K Expeditions Offers Guided Trips in the Rogers Pass Area and beyond</a>
+            </div>
             
-            </div>
-            <div className="text-right text-4xl m-8">
-                <a className=" hover:text-white" href="https://glacierraft.com">Glacier Rafting, Golden BC</a><br />
-                <a className=" hover:text-white" href="https://www.maurmere.com">Maur Mere Media,Golden BC </a><br />
-                <a className=" hover:text-white" href="https://www.pc.gc.ca/en/pn-np/bc/glacier">Glacier National Park Website</a><br />
-                <a className=" hover:text-white" href="https://blackfeather.com/hike">Black Feather The wilderness adventure company</a><br />
-                <a className=" hover:text-white" href="https://heathermountainlodge.com">Heater Mountain Lodge Accomdation just outside out Glacier</a><br />
-                <a className=" hover:text-white" href="https://skookumcycleandski.com">Skookum Cycle and Ski, Revelstoke BC</a><br />
-                <a className=" hover:text-white" href="https://www.sandkexpeditions.com">S and K Expeditions Offers Guided Trips in the Rogers Pass Area and beyond</a>
-            </div>
-
+            
+ 
         </Fragment>
     )
 }
