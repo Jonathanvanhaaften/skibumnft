58bf4dde657de85555b34119a548969e8c0fd74f
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2021-12-09 15:57:36 -0700

    set up new changes

--- skibumnft/src/components/content/Gallery.js
+++ skibumnft/src/components/content/Gallery.js
@@ -10,41 +10,41 @@ const Gallery = () => {
            <div className="md:grid md:grid-col-6 md:grid-rows-3 sm:mb-20 mr-8">
             <div className="col-start-1 
              w-full h-full flex justify-center m-4  sm:pb-20 row-spam-2">
-                <Picture/>
+                <Picture picture={true} setPic={false}/>
             </div>
             <div className="col-start-2 
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                <Picture/>
+                <Picture setPic={false}/>
             </div>
                     <div className="col-start-3 
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                        <Picture/>
+                        <Picture setPic={false}/>
                     </div>
                     {/* Start of row 2 */}
                     <div className="col-start-1 row-start-2
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                <Picture/>
+                <Picture setPic={false}/>
             </div>
             <div className="col-start-2 row-start-2
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                <Picture/>
+                <Picture setPic={false}/>
             </div>
                     <div className="col-start-3 row-start-2
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                        <Picture/>
+                        <Picture setPic={false}/>
                     </div>
                        {/* Start of row 3 */}
                        <div className="col-start-1 row-start-3
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                <Picture/>
+                <Picture setPic={false}/>
             </div>
             <div className="col-start-2 row-start-3
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                <Picture/>
+                <Picture setPic={false}/>
             </div>
                     <div className="col-start-3 row-start-3
              w-full h-full flex justify-center m-4 sm:pb-20 row-spam-2">
-                        <Picture/>
+                        <Picture setPic={false}/>
                     </div>
                 </div>
             </div>
--- skibumnft/src/components/content/Picture.js
+++ skibumnft/src/components/content/Picture.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import  bgimg from '../../images/bgimg.jpg'
 
-const Picture = props => {
+const Picture = ({picture,setPic}) => {
+  if (setPic  ){
+    return(
+    <div className=" w-full h-full p-4 m-14 bg-fixed bg-cover rounded relative flex justify-center " style={{ backgroundImage: `url('${bgimg}` }}>
+    <img className='inline-block' src={picture} alt="" /> </div>
+         )
+  }else{
     return (
-            <div className=" w-full h-full p-4 m-14 bg-fixed bg-cover rounded relative flex justify-center " style={{ backgroundImage: `url('${bgimg}` }}>
+      
+  <div className=" w-full h-full p-4 m-14 bg-fixed bg-cover rounded relative flex justify-center " style={{ backgroundImage: `url('${bgimg}` }}>
           <div className="bg-black w-3/4 text-9xl ">
+         
 <p className=" flex justify-center text-white md:pt-40">?</p>
           </div>
         </div> 
-    );
+    );}
 };
-
+// {if picture = undefined then ? else image }
 export default Picture;
