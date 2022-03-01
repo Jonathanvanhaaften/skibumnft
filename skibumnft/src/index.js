466e3c6214322b4ab9af9fa84c1b61f11f710c42
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2022-03-01 11:06:39 -0700

    trying react snap

--- skibumnft/src/index.js
+++ skibumnft/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/main.css';
 import App from './App';
-
+import { hydrate, render } from "react-dom";
 
 import { Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
@@ -12,7 +12,8 @@ const alertOptions = {
   position: "top center"
 };
 
-ReactDOM.render(
+// ReactDOM.render
+const APP = (
   <AlertProvider template={AlertTemplate}{...alertOptions}>
   <div>
     <App/>
@@ -20,6 +21,13 @@ ReactDOM.render(
   </div></AlertProvider>,
   document.getElementById('root')
 );
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App/>, rootElement);
+} else {
+  render(<App/>, rootElement);
+}
 
 
 
