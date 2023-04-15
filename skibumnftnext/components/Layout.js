e10ddfe476891f42b7a93b46fe99232d260eb258
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2023-04-15 13:12:41 -0600

    fixed white line (blank space) at the bottom pages

--- skibumnftnext/components/Layout.js
+++ skibumnftnext/components/Layout.js
@@ -5,16 +5,18 @@ import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <Analytics>
-      <Navbar />
-      <div className="flex justify-center bg-black">
-        <img
-          className="  lg:h-87 md:h-90"
-          src="/hero5.png"
-          alt="Hero Title Image Ski Bum NFT"
-        />
+      <div className="bg-black">
+        <Navbar />
+        <div className="flex justify-center bg-black">
+          <img
+            className="  lg:h-87 md:h-90"
+            src="/hero5.png"
+            alt="Hero Title Image Ski Bum NFT"
+          />
+        </div>
+        {children}
+        <Footer />
       </div>
-      {children}
-      <Footer />
     </Analytics>
   );
 };
